35d27598dd9a639c719db79680568853d86bf43c
ishikanawar <ISHIKA.NAWAR20@stu-mail.ccny.cuny.edu>
2026-09-03 00:39:40 -0400

    Add files via upload

--- ecob2000_lecture_1_household_pulse_ishika_nawar.R
+++ ecob2000_lecture_1_household_pulse_ishika_nawar.R
@@ -125,4 +125,133 @@ Min. 1st Qu.  Median    Mean 3rd Qu.    Max.
 [1] 53.28593
 > sd(Age[Gender == "male"])
 [1] 16.28551
-> 
+> hist(Age[ (Age > 80) ])
+mean(Age[ (Gender == "female") & (Age < 88) ]) 
+## [1] 51.51615
+summary(Education)
+##        lt hs      some hs  high school some college    assoc deg college grad 
+##         6787        14934       122541       210698       103575       279400 
+##   adv degree 
+##       246855
+summary(State)
+##              Alabama               Alaska              Arizona 
+##                13230                15970                26059 
+##             Arkansas           California             Colorado 
+##                12931                71958                24746 
+##          Connecticut             Delaware District of Columbia 
+##                16499                11555                13008 
+##              Florida              Georgia               Hawaii 
+##                33825                23538                10694 
+##                Idaho             Illinois              Indiana 
+##                18517                22348                17907 
+##                 Iowa               Kansas             Kentucky 
+##                15547                17328                14104 
+##            Louisiana                Maine             Maryland 
+##                12481                10524                22276 
+##        Massachusetts             Michigan            Minnesota 
+##                26236                26479                21160 
+##          Mississippi             Missouri              Montana 
+##                10391                16868                11519 
+##             Nebraska               Nevada        New Hampshire 
+##                15248                15658                15337 
+##           New Jersey           New Mexico             New York 
+##                18603                17666                18996 
+##       North Carolina         North Dakota                 Ohio 
+##                17425                 8649                16312 
+##             Oklahoma               Oregon         Pennsylvania 
+##                15025                24682                24969 
+##         Rhode Island       South Carolina         South Dakota 
+##                 9783                14910                 9936 
+##            Tennessee                Texas                 Utah 
+##                16977                49059                25831 
+##              Vermont             Virginia           Washington 
+##                10607                25432                37615 
+##        West Virginia            Wisconsin              Wyoming 
+##                11142                17272                 9958
+d_HHP2020_24 %>%
+  group_by(State) %>%
+  summarize(
+    avg = mean(Age),
+    stdev = sd(Age), 
+    n_obs = n()
+  ) 
+## # A tibble: 51 × 4
+##    State                  avg stdev n_obs
+##    <fct>                <dbl> <dbl> <int>
+##  1 Alabama               52.5  16.0 13230
+##  2 Alaska                50.9  15.8 15970
+##  3 Arizona               54.4  16.4 26059
+##  4 Arkansas              52.2  16.0 12931
+##  5 California            52.0  15.9 71958
+##  6 Colorado              51.5  16.1 24746
+##  7 Connecticut           52.7  15.8 16499
+##  8 Delaware              55.5  16.0 11555
+##  9 District of Columbia  48.4  15.5 13008
+## 10 Florida               55.7  16.0 33825
+## # ℹ 41 more rows
+d_HHP2020_24 %>%
+  group_by(State) %>%
+  summarize(
+    age90th = quantile(Age,probs = 0.9),
+    age10th = quantile(Age,probs = 0.1), 
+    n_obs = n()
+  ) %>%
+  arrange(desc(age90th), .by_group = TRUE)
+## # A tibble: 51 × 4
+##    State          age90th age10th n_obs
+##    <fct>            <dbl>   <dbl> <int>
+##  1 Florida             76      33 33825
+##  2 Arizona             75      31 26059
+##  3 Delaware            75      33 11555
+##  4 Hawaii              75      33 10694
+##  5 New Mexico          75      33 17666
+##  6 Maine               74      32 10524
+##  7 Montana             74      31 11519
+##  8 Nevada              74      32 15658
+##  9 New Hampshire       74      32 15337
+## 10 South Carolina      74      31 14910
+## # ℹ 41 more rows
+table(Education,Gender)
+##               Gender
+## Education        male female  trans  other
+##   lt hs          2847   3727     64    149
+##   some hs        5752   9003     45    134
+##   high school   48030  73467    243    801
+##   some college  85218 123645    572   1263
+##   assoc deg     37020  65834    145    576
+##   college grad 122956 154321    564   1559
+##   adv degree   108713 136467    356   1319
+xtabs(~ Education + Gender)
+##               Gender
+## Education        male female  trans  other
+##   lt hs          2847   3727     64    149
+##   some hs        5752   9003     45    134
+##   high school   48030  73467    243    801
+##   some college  85218 123645    572   1263
+##   assoc deg     37020  65834    145    576
+##   college grad 122956 154321    564   1559
+##   adv degree   108713 136467    356   1319
+prop.table(table(Education,Gender))
+##               Gender
+## Education              male       female        trans        other
+##   lt hs        2.890972e-03 3.784563e-03 6.498847e-05 1.513013e-04
+##   some hs      5.840839e-03 9.142051e-03 4.569502e-05 1.360696e-04
+##   high school  4.877182e-02 7.460169e-02 2.467531e-04 8.133714e-04
+##   some college 8.653418e-02 1.255547e-01 5.808345e-04 1.282507e-03
+##   assoc deg    3.759177e-02 6.685080e-02 1.472395e-04 5.848963e-04
+##   college grad 1.248550e-01 1.567045e-01 5.727109e-04 1.583079e-03
+##   adv degree   1.103921e-01 1.385747e-01 3.614984e-04 1.339372e-03
+
+mean(Age[(Region == "Northeast")])
+## [1] 52.48172
+# alternatively
+restrict1 <- as.logical((Region == "Northeast"))
+dat_northeast <- subset(d_HHP2020_24, restrict1)
+
+detach()
+attach(dat_northeast)
+
+mean(Age)
+## [1] 52.48172
+
+
